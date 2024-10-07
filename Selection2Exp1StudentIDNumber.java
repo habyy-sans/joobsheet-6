133e5dafd3c876fc3c380e5bcf54fee50bec1eb5
habyy-sans <muhammadhasbiyashshiddiqi@gmail.com>
2024-10-07 12:28:37 +0700

    soal ke 4

--- Selection2Exp1StudentIDNumber.java
+++ Selection2Exp1StudentIDNumber.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
+
 public class Selection2Exp1StudentIDNumber {
     public static void main(String[] args) {
         Scanner inputStudentIDNumber = new Scanner(System.in);
         System.out.print("Input year: ");
         int year = inputStudentIDNumber.nextInt();
         if (year % 4 == 0) {
-            if (year % 100 != 0) {
-                System.out.println("leap year");
-            } else {
+            if (year % 100 == 0) {
                 if (year % 400 == 0) {
-                    System.out.println("leap year");
+                    System.out.println("Leap year");
                 } else {
                     System.out.println("Not a leap year");
                 }
+            } else {
+                System.out.println("Leap year");
             }
         } else {
             System.out.println("Not a leap year");
         }
     }
-}
+} 
